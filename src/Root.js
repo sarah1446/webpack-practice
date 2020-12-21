5bca83c5959dc7bca0b7a2accc7948a248fc4287
sarah1446 <lemonview2@gmail.com>
2020-12-21 23:48:58 +0900

    scss까지 빌드완료

--- src/Root.js
+++ src/Root.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './style.scss';
 
 const Root = () => {
   return (
-    <h3>Hello, React!!!!zz</h3>
+    <h3 className="title">!!Hello, React!!!!zz</h3>
   );
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -7,7 +8,13 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname + "/build")
   },
+  devServer: {
+    contentBase: path.resolve("./build"),
+    index: "index.html",
+    port: 9000
+  },
   mode: "development",
+  
   module: {
     rules: [
       {
@@ -23,6 +30,14 @@ module.exports = {
             options: { minimize: false }
           }
         ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
       }
     ]
   },
@@ -30,6 +45,9 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: './public/index.html', // public/index.html 파일을 읽는다.
       filename: 'index.html' // output으로 출력할 파일은 index.html 이다.
-    })
+    }),
+    new MiniCssExtractPlugin({
+        filename: 'style.css'
+      })
   ]
 };
