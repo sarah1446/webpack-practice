2afc944ecb54e492c738d2d2f0dc75a6e15aac5e
sarah1446 <lemonview2@gmail.com>
2020-12-21 11:31:46 +0900

    webpack js파일 빌드

---
+++ src/Root.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Root = () => {
+  return (
+    <h3>Hello, React!!!!zz</h3>
+  );
+};
+
+export default Root;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname + "/build")
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: "/node_modules",
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: false }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html', // public/index.html 파일을 읽는다.
+      filename: 'index.html' // output으로 출력할 파일은 index.html 이다.
+    })
+  ]
+};
