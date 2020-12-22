b46212cb0824202a65e69f88db7f6d8548dad5b5
ian <seunghyun.pan@ssgsag.kr>
2020-12-22 18:20:16 +0900

    Initialize typescript with web-pack.
    n/a

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,58 @@
-const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname + "/build")
-  },
-  devServer: {
-    contentBase: path.resolve("./build"),
-    index: "index.html",
-    port: 9000
-  },
-  mode: "development",
-  
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: "/node_modules",
-        use: ['babel-loader'],
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: '/node_modules/',
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader",
-            options: { minimize: false }
+            loader: 'html-loader',
+            options: {
+              minimize: false,
+            },
           }
         ]
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       }
     ]
   },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    index: 'index.html',
+    port: 9000,
+  },
+  mode: 'development',
   plugins: [
     new HtmlWebPackPlugin({
-      template: './public/index.html', // public/index.html 파일을 읽는다.
-      filename: 'index.html' // output으로 출력할 파일은 index.html 이다.
+      template: './public/index.html',
+      filename: 'index.html',
     }),
     new MiniCssExtractPlugin({
-        filename: 'style.css'
-      })
-  ]
+      filename: 'style.css',
+    }),
+  ],
 };
